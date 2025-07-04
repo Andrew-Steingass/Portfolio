66464bee1be2667ab6f25ccb8730ea431d65eb89
Andy Steingass <AndrewSteingass@gmail.com>
2025-07-04 03:09:36 +0000

    Add NumPy array addition example

---
+++ hello.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np
+
+
+print("Hello from Andy!")
+a = np.array([1, 2, 3])
+b = np.array([4, 5, 6])
+
+# Add them
+c = a + b
+
+print("Array A:", a)
+print("Array B:", b)
+print("A + B =", c)
